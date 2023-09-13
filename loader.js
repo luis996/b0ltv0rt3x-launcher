e73a040fe177142a14a12d17db2f165e62b4f248
Luis64_gamer <luiswilfredo1004@gmail.com>
2023-09-12 21:42:58 -0500

    fixed dumb bug

--- loader.js
+++ loader.js
@@ -177,7 +177,7 @@ const launchCustomForge = async (username, forge, version) => {
       // the getAuth function through the authorization field and instead
       // handling authentication outside before you initialize
       // MCLC so you can handle auth based errors and validation!
-      // javaPath: `${installRoot}/java8/bin/java.exe`,
+      javaPath: `${installRoot}/java${java}/bin/java.exe`,
       authorization: Authenticator.getAuth(username, ""),
       root: installRoot,
       forge: path.join(installRoot) + '/' + forge + '.jar',
